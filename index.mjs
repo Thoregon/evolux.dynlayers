d4d984d5888832d0a992af5260edaf9b891a9eed
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-11-25 10:29:51 +0100

    common logging

---
+++ index.mjs
@@ -0,0 +1,45 @@
+/**
+ *
+ *
+ * @author: Bernhard Lukassen
+ */
+
+import { myevolux }     from '/evolux.universe';
+
+//**** now define all standard exports
+
+export { default as Layer }        from './lib/layer.js';
+
+export const service = {
+    install() {
+        console.log('** matter install()');
+        myevolux().matter = new Matter();
+    },
+
+    uninstall() {
+        console.log('** matter uninstall()');
+        delete myevolux().matter;
+    },
+
+    resolve() {
+        console.log('** matter resolve()');
+        // nothing to do
+    },
+
+    start() {
+        console.log('** matter start()');
+        myevolux().matter.condense();
+    },
+
+    stop() {
+        console.log('** matter stop()');
+        myevolux().matter.freeze();
+    },
+
+    update() {
+        console.log('** matter update()');
+        this.stop();
+        this.uninstall();
+        this.install();
+    }
+};

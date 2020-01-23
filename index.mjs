e09c467e8fc23ca6d7b156b331903bcbcd8c114b
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-01-23 15:40:41 +0100

    matter: remove gun layers, work on matter access

--- index.mjs
+++ index.mjs
@@ -7,13 +7,18 @@
 import { myuniverse, tservices } from '/evolux.universe';
 import Layers                   from './lib/layers.mjs';
 
-//**** now define all standard exports
+// **** now define all standard exports
 export { default as Layer }         from './lib/routing/layer.mjs';
 export { default as Step }          from './lib/routing/step.mjs';
 export { default as Parallel }      from './lib/routing/parallel.mjs';
 export { default as Switch }        from './lib/routing/switch.mjs';
 
+// **** simple monitoring layer implementation
 export { default as MonitorLayer }  from './lib/util/monitorlayer.mjs';
+
+// ****
+// export { default as Request }                   from './lib/request.mjs';
+// export { default as Response, RESPONSE_STATI }  from './lib/response.mjs';
 
 export const service = {
     install() {
